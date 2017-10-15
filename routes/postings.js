f7bb24994d9b70a8049847d56e7c1fda8da73e70
Damindu <damindu7@gmail.com>
2017-10-15 16:00:21 -0400

    Removed all old funtions which uses primary id

--- routes/postings.js
+++ routes/postings.js
@@ -16,7 +16,6 @@ router.post('/', function (req, res, next) {
   });
 });
 
-
 /* GET list all job postings */
 router.get('/', function (req, res, next) {
   JobPosting.find({}, function (err, postings) {
@@ -46,32 +45,6 @@ router.get('/:postingId', function(req, res, next){
     });
 });
 
-/* GET show job posting */
-// router.get('/:_id', function (req, res, next) {
-//   JobPosting.findOne(req.params._id, function (err, posting) {
-//     if (err) {
-//       res.status(500).json({error: err});
-//     } else {
-//       res.status(200).json(posting);
-//     }
-//   });
-// });
-
-/* GET job posting using postingId */
-// router.get('/:_id', function(req, res, next){
-//   JobPosting.findOne({postingId: req.params._id},
-//     function(err, posting){
-//       if(err){
-//         res.status(500);
-//         console.log('did not get it!');
-//       } else {
-//         res.status(200).json(posting);
-//         console.log('got it!');
-//       }
-//     });
-// });
-
-
 /* DELETE QUERY romoves a posting with postingId */
 router.delete('/:postingId', function (req, res, next) {
   JobPosting.
@@ -89,18 +62,6 @@ router.delete('/:postingId', function (req, res, next) {
     });
 });
 
-
-/* DELETE remove a posting with primary Id */
-// router.delete('/:_id', function (req, res, next) {
-//   JobPosting.findByIdAndRemove(req.params._id, function (err, posting) {
-//     if (err) {
-//       res.status(500).json({error: err});
-//     } else {
-//       res.status(200).json(posting);
-//     }
-//   });
-// });
-
 /*Update specific posting with PUT QUERY */
 router.put('/:postingId', function (req,res){
   JobPosting.
@@ -116,26 +77,6 @@ router.put('/:postingId', function (req,res){
       }
     });
 });
-
-/*Update specific posting */
-// router.put('/:_id', function (req, res){
-//   JobPosting.findOneAndUpdate(
-//   {postingId: req.params._id},
-//   {$set: {title: req.body.title}},
-//   {upsert: true},
-//     function(err, newPosting){
-//       if(err){
-//         res.status('An error');
-//         res.status(500);
-//       } else {
-//         console.log(newPosting);
-//         console.log('sent OK');
-//         res.status(200);
-//       }
-//   });
-// });
-                                                
-
 
 /* GET remove all postings */
 /* This is useful for testing purposes */
